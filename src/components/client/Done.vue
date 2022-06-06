6006480f736ddeea2c88a980d5d13027809ab7ac
ikramdhib <dhibikram50@gmail.com>
2022-06-06 13:16:02 +0100

    comment

--- src/components/client/Done.vue
+++ src/components/client/Done.vue
@@ -37,7 +37,7 @@
           <div class="col-md-10 heading-section text-center"> 
               <h1>
                   terminé</h1>
-                  <!---------------hh--------------------->
+                  <!---------------hhhhhhhhhhhhhhhhhhhhhhhhh--------------------->
           </div>
           </div>
           <div class="row justify-content-center mb-9 pb-2">
